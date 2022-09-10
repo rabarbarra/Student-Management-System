c955b81a9638907ce3861aaee3ac31be91a61f63
basia <barbaramanturzyk>
2022-09-10 16:02:17 +0200

    Added an update student functionality

--- students/views.py
+++ students/views.py
@@ -36,3 +36,20 @@ def add(request):
     else:
         form = StudentForm()
     return render(request, 'students/add.html', {'form': StudentForm()})
+
+def edit(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/edit.html', {
+                'form': form,
+                'success': True
+            })
+    else:
+        student = Student.objects.get(pk=id)
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit.html', {
+        'form': form
+    })
